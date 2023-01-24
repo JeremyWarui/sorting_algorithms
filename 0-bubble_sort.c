544856269e64a04d234363318358c9311e8c41d2
JeremyWarui <jerywarne7@gmail.com>
2023-01-24 23:23:12 +0300

    BUbble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -23,24 +23,20 @@ void swap_ints(int *a, int *b)
 */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, len = size;
-	bool bubbly = false;
+	size_t i, j, len = size;
 
 	if (array == NULL || size < 2)
 		return;
 
-	while (bubbly == false)
+	for (i = 0; i < len - 1; i++)
 	{
-		bubbly = true;
-		for (i = 0; i < len - 1; i++)
+		for (j = 0; j < len - i - 1; j++)
 		{
-			if (array[i] > array[i + 1])
+			if (array[j] > array[j + 1])
 			{
-				swap_ints(array + i, array + i + 1);
+				swap_ints(array + j, array + j + 1);
 				print_array(array, size);
-				bubbly = false;
 			}
 		}
-		len--;
 	}
 }
